3c39b843b18e53e784e3a0d39fcb70841dba0add
linxTeng <linxiangboy@163.com>
2018-04-18 22:58:55 +0800

    提供一个mvc的mock用例

---
+++ hoho-web-user/src/main/java/com/hoho/user/controller/ControllerTest.java
@@ -0,0 +1,38 @@
+package com.hoho.user.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("classpath:/META-INF/spring/*.xml")
+public class ControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testaa() throws Exception {
+        this.mockMvc.perform(get("/page/ldist2")).andDo(MockMvcResultHandlers.print()).andExpect(status().isOk());
+    }
+}
+
+
--- hoho-web-user/src/main/java/com/hoho/user/controller/HomeController.java
+++ hoho-web-user/src/main/java/com/hoho/user/controller/HomeController.java
@@ -1,14 +1,28 @@
 package com.hoho.user.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @RequestMapping("/home")
     public String home(HttpServletRequest request) {
         return "home";
+    }
+
+    @RequestMapping("/page/list")
+    @ResponseBody
+    public List<Integer> getBody() {
+        List<Integer> aa = new ArrayList<>();
+        aa.add(1);
+        aa.add(2);
+        aa.add(3);
+        return aa;
     }
 }
