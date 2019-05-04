7d18e96a897cb37c9bb5d5a33408700bcba399cf
linxTeng <linxiangboy@163.com>
2019-05-04 18:44:08 +0800

    新增动态代理模式的用例

---
+++ hoho-mod-test/src/test/java/com/hoho/test/mode/ProxyPattern/PersonBean.java
@@ -0,0 +1,9 @@
+package com.hoho.test.mode.ProxyPattern;
+
+public interface PersonBean {
+    String getName();
+    String getGender();
+
+    void setName(String name);
+    void setGender(String gender);
+}
